e1324322849e3feb3ba7c045326181c1a3b79cce
rokor12 <rodrigo181299@gmail.com>
2022-08-01 23:48:46 -0300

    Sort Array By Parity II

---
+++ sort-array-by-parity-ii.c
@@ -0,0 +1,41 @@
+// leetcode.com/problems/sort-array-by-parity-ii/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int* sortArrayByParityII(int* nums, int numsSize, int* returnSize);
+
+int main(){
+    int const numsSize = 4;
+    int arr[4] = {4,2,5,7};
+    int* nums = (int*)malloc(numsSize*sizeof(int));
+    memcpy(nums,arr,numsSize*sizeof(int));
+    int* ans;
+    int* returnSize = malloc(sizeof(int));
+    sortArrayByParityII(nums, numsSize, returnSize);
+    for(int i=0;i<numsSize;i++){
+        printf("%d ",nums[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+int* sortArrayByParityII(int* nums, int numsSize, int* returnSize){
+    *returnSize = numsSize;
+    int* ans = (int*)malloc(numsSize*sizeof(int));
+    int i,j,aux;
+    for(i=0;i<numsSize;i++){
+        if((i%2 != nums[i]%2)){
+            for(j=i+1;j<numsSize;j++){
+                if (i%2 == nums[j]%2){
+                    aux = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = aux;
+                }
+            }
+        }
+        ans[i] = nums[i];
+    }
+    return ans;
+}
