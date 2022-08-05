06a1a4d545f4a8c3c51cacda285070bc036cdc7b
rokor12 <rodrigo181299@gmail.com>
2022-08-05 05:07:40 -0300

    Intersection Of Multiple Arrays

---
+++ intersection-of-multiple-arrays.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int* intersection(int **nums, int numsSize, int *numsColSize, int *returnSize);
+
+int main(){
+    int numsSize = 3;
+    int numsColSize[3]={5,4,4};
+    int **nums = (int**)malloc(numsSize*sizeof(int*));
+    int i,j;
+    int *aux;
+    int numsA[3][10] = {{3,1,2,4,5},{1,2,3,4},{3,4,5,6}};
+    for(i=0;i<numsSize;i++){
+        aux = (int*)malloc(numsColSize[i]*sizeof(int));
+        memcpy(aux,numsA[i],numsColSize[i]*sizeof(int));
+        nums[i] = aux;
+    }
+    int *returnSize = malloc(sizeof(int));
+    int *ans = intersection(nums, numsSize, numsColSize, returnSize);
+    for(int i=0;i<*returnSize;i++){
+        printf("%d ",ans[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+int* intersection(int **nums, int numsSize, int *numsColSize, int *returnSize){
+    int *ans = (int*)malloc(sizeof(int));
+    int i,j,k,c,size=0;
+    int aux;
+    for(i=0;i<numsColSize[0];i++){
+        aux = nums[0][i];
+        c=1;
+        for(j=1;j<numsSize;j++){
+            for(k=0;k<numsColSize[j];k++){
+                if(aux == nums[j][k]){
+                    c++;
+                }
+            }
+            if(c == numsSize){
+                size++;
+                ans = realloc(ans,size);
+                ans[size-1] = aux;
+            }
+        }
+    }
+    *returnSize = size;
+    return ans;
+}
