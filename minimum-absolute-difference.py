96cb34473aaaa828fc1bf04bd84e2c912d7ab278
rokor12 <rodrigo181299@gmail.com>
2022-08-05 02:30:19 -0300

    Minimum Absolute Difference

--- minimum-absolute-difference.py
+++ minimum-absolute-difference.py
@@ -1,11 +1,17 @@
 def main():
     arr = [4,2,1,3]
+    ans = minimumAbsDifference(arr)
+    print(ans)
 
 def minimumAbsDifference(arr):
     arr.sort()
     minAbsDif = arr[1] - arr[0]
     ans = []
     for i in range(len(arr)-1):
-        for j in range(i+1,len(arr)):
-            if arr[j]-arr[i] == minAbsDif:
-                ans.append([arr[0],arr[i]])
+        if arr[i+1] - arr[i] < minAbsDif:
+            minAbsDif = arr[i+1] - arr[i]
+            ans = [[arr[i],arr[i+1]]]
+        elif arr[i+1]-arr[i] == minAbsDif:
+            ans.append([arr[i],arr[i+1]])
+    return ans
+main()
