7ee21ac4539dc4d833b334d6b4d5a3433e2f6271
rokor12 <rodrigo181299@gmail.com>
2022-08-17 15:03:13 -0300

    Maximum Number Of Pairs In Array

--- maximum-number-of-pairs-in-array.c
+++ maximum-number-of-pairs-in-array.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -8,13 +9,13 @@ int notInArray(int num, int *arr, int arrSize);
 
 int main(){
     //[1,3,2,1,3,2,2]
-    int aux[7] = {1,3,2,1,3,2,2};
+    int aux[46] = {5,12,53,22,7,59,41,54,71,24,91,74,62,47,20,14,73,11,82,2,15,38,38,20,57,70,86,93,38,75,94,19,36,40,28,6,35,86,38,94,4,90,18,87,24,22};
     int *nums;
     nums = aux;
-    int numsSize = 7;
+    int numsSize = 46;
     int *returnSize = (int*)malloc(sizeof(int));
     int *res = numberOfPairs(nums, numsSize, returnSize);
-    printf("[%d, %d] %d\n",res[0],res[1],*returnSize);
+    printf("[%d, %d]\n",res[0],res[1]);
     return 0;
 }
 
@@ -50,12 +51,11 @@ int* numberOfPairs(int* nums, int numsSize, int* returnSize){
         auxSize++;
         aux = realloc(aux,auxSize*sizeof(int));
         aux[auxSize-1] = nums[i];
-        
         c = c/2;
         
         if(c>0){
             auxNumsSize -= c*2;
-            res[0]++;
+            res[0] += c;
         }
     }
     res[1] = auxNumsSize;
