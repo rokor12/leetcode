6a3f054d28c2d346968de4642fd7cab561c46bff
rokor12 <rodrigo181299@gmail.com>
2022-08-31 10:52:44 -0300

    Make Array Zero By Subtracting Equal Amounts

--- make-array-zero-by-subtracting-equal-amounts.c
+++ make-array-zero-by-subtracting-equal-amounts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum { false, true } bool;
 
@@ -13,24 +14,26 @@ int main(){
 }
 
 int minimumOperations(int* nums, int numsSize){
+    int *aux = (int*)malloc(numsSize*sizeof(int));
+    memcpy(aux, nums, numsSize*sizeof(int));
     bool flag = true,flag2;
     int i,mini,c=0;
     while(flag){
         flag = false;
-        mini = nums[0];
+        mini = aux[0];
         for(i=0;i<numsSize;i++){
-            if(nums[i]!=0){
+            if(aux[i]!=0){
                 flag = true;
-                if(mini==0 || nums[i]<mini){
-                    mini = nums[i];
+                if(mini==0 || aux[i]<mini){
+                    mini = aux[i];
                 }
             }
         }
         if(flag){
             c++;
             for(i=0;i<numsSize;i++){
-                if(nums[i]!=0){
-                    nums[i]-=mini;
+                if(aux[i]!=0){
+                    aux[i]-=mini;
                 }
             }
         }
